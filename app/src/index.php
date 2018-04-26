af4d6fe0062b8f94a216b0066de5a6a598da5907
Lionel Legrand <llegrand@lusis.lu>
2018-04-26 17:22:05 +0200

    first commit

---
+++ app/src/index.php
@@ -0,0 +1,35 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test Docker-compose</title>
+    </head>
+
+    <body>
+        <?php 
+            $bdd = null; 
+            while($bdd == null){
+                try{
+                    $bdd = new PDO('mysql:host=db:3306;dbname=app', 'app', 'app');
+                    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch(Exception $e){
+                    sleep(10);
+                }
+            }
+            $bdd->exec("CREATE TABLE IF NOT EXISTS test(
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                libelle VARCHAR(40) NOT NULL);"
+            );
+
+            $bdd->exec("INSERT INTO test(libelle) VALUES('Libelle dans la base');");
+
+            $req = $bdd->query("SELECT * FROM test;")->fetch();
+
+
+            echo $req['libelle'];
+        ?>
+    </body>
+</html>
+
