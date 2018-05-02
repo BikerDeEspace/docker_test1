ac3a6ae358ed4b739a48c6f084703c7d365bc492
Lionel Legrand <llegrand@lusis.lu>
2018-05-02 11:33:12 +0200

    Change BDD container

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # docker_test1
 Application multi containers Apache / MySQL
+
+Ports: 80
+
+#Containers
+##Application principale
+Nom: app
+Ports: 80
+Image: php:7.0-apache
+
+##Base de données
+Nom: bdd
+Image: mysql:5.7
+Port: 3306
--- app/src/index.php
+++ app/src/index.php
@@ -10,21 +10,30 @@
     <body>
         <?php 
             $bdd = null; 
+            //Try connect to database 
             while($bdd == null){
                 try{
                     $bdd = new PDO('mysql:host=db:3306;dbname=app', 'app', 'app');
                     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 } catch(Exception $e){
+                    echo "<script>console.log( 'Initialisation base de données' );</script>";
                     sleep(10);
                 }
             }
+
+            //Create table if not exist
             $bdd->exec("CREATE TABLE IF NOT EXISTS test(
                 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 libelle VARCHAR(40) NOT NULL);"
             );
+
+            //Insert test element
             $bdd->exec("INSERT INTO test(libelle) VALUES('Libelle dans la base');");
             
+            //Get test element
             $req = $bdd->query("SELECT * FROM test;")->fetch();
+            
+            //print test element
             echo $req['libelle'];
         ?>
     </body>
